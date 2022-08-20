99f40f0fe3ff666e3711931d9d7ef16e6827fee3
Christy Le <christylex3@gmail.com>
2022-08-19 21:24:44 -0400

    Added delete request to delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,5 +39,15 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
-    
-}
+    // Delete a user and their thoughts
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with that ID!" })
+                    : User.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: "User and their thoughts are deleted!" }))
+			.catch((err) => res.status(500).json(err));
+    }, 
+};
