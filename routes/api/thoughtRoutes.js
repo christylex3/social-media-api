24a24283e437758fb048014c509276aa82beed9b
Christy Le <christylex3@gmail.com>
2022-08-20 01:57:22 -0400

    Added reactions' post and delete routes in thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -20,4 +20,10 @@ router
     .put(updateThought)
     .delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
 module.exports = router;
