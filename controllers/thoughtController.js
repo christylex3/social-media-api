4b5fd7f08960c2e99be87446d428821f63e6a2d2
Christy Le <christylex3@gmail.com>
2022-08-19 23:31:08 -0400

    Added PUT request to update thought by finding its id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -40,6 +40,23 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
-    
+    // Updates thought by finding its id
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: "No thought with this id!"})
+                : res.json(thought)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
 
 };
